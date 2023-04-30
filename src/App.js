892acbeb99f49508c12ce7840365cbdbbd4a0e0b
Arghya <arghyadas242004@gmail.com>
2023-04-30 10:16:30 +0530

    Add: Authorization and Copy button Done

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ function App() {
 
   const [WarnText, setWarnText] = useState("Enter 5 letters") ;
   const [LinkTxt, setLinkTxt] = useState("") ;
+  const [Auth, setAuth] = useState(false) ;
 
   let handleKey = (event) => {
     const str = (event.target.value) ;
-    if (str.length < 5)
+    if (str.length < 5){
       setWarnText("Enter 5 letters") ;
-    else if (str.length === 5)
+      setAuth(false) ;
+    }
+    else if (str.length === 5){
       setWarnText("Perfect") ;
-    else
+      setAuth(true) ;
+    }
+    else{
       setWarnText("The Word is too big") ;
+      setAuth(false) ;
+    }
   }
 
   let generateLink = () => {
+    if (Auth === false){
+      setWarnText("The Word should have 5 letters") ;
+      setLinkTxt("") ;
+      return;
+    }
+
     const inputElem = document.getElementsByTagName('input')[0] ;
     let val = inputElem.value ;
 
@@ -26,27 +39,30 @@ function App() {
       let charAscii = val.charCodeAt(i) - 97 ;
       if (charAscii <= 9 ) {
         str+=`0${charAscii}` ;
-        console.log(`${val[i]} - 0${charAscii}`) ;
       }
       else{
         str+=`${charAscii}` ;
-        console.log(`${val[i]} - ${charAscii}`) ;
       }
     }
 
-    setLinkTxt(`${window.location.href}/${str}`) ;
+    setLinkTxt(`${window.location.href}${str}`) ;
   }
 
+  let copyClip = () => {
+    navigator.clipboard.writeText(LinkTxt) ;
+  }
 
-  // Authentication and Copy Button Left
   return (
     <div className="App flex-col">
       <h1>Welcome to Wordle multiplayer</h1>
       <h4>Enter a Word</h4>
       <input type="text" onChange={handleKey} />
-      <p>{WarnText}</p>
-      <button onClick={generateLink}>Generate Link</button>
-      <p className="link">{LinkTxt}</p>
+      <p className='warn-text' style={{color : Auth? 'green':'red'}} >{WarnText}</p>
+      <button className='gen-btn' onClick={generateLink}>Generate Link</button>
+      <div style={{ visibility : (LinkTxt.length!==0)? 'visible':'hidden' }} className="link flex-row">
+        <p>{LinkTxt}</p>
+        <button className='cpy-btn' style={{ visibility : (LinkTxt.length!==0)? 'visible':'hidden' }} onClick={copyClip} >🔗copy</button>
+      </div>
     </div>
   );
 }
