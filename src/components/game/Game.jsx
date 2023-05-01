fb56b5894ccb2d9f0d03f33de12166dfc338ec63
Arghya <arghyadas242004@gmail.com>
2023-05-01 16:39:48 +0530

    Add: Winner portion complete

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -26,7 +26,10 @@ let rowCount = 0;
 const Game = () => {
   const [Matrix, setMatrix] = useState([...wordMat]) ;
   const [BgColor, setBgColor] = useState([...colorMat]) ;
-  const [Terminate, setTerminate] = useState(0) ;
+  const [Sigmoid, setSigmoid] = useState(0) ;
+  const [Para, setPara] = useState([0,"Insert a 5 Letter Word"])
+
+
   const inputRef = useRef(null);
 
   let { game } = useParams() ;
@@ -36,15 +39,27 @@ const Game = () => {
     word+= String.fromCharCode(num) ;
   }
 
+  let win = (won,str) => {
+    if (won){
+      setPara([1,str]) ;
+    }
+    else{
+      setPara([-1,str]) ;
+    }
+  }
+
+
   let insertRow = () => {
     wordMat = Matrix ;
     colorMat = BgColor ;
-    let inputWord = inputRef.current.value ;
+    const inputWord = (inputRef.current.value).toLowerCase() ;
+    let match = 0;
     
     for (let j=0; j<5; j++){
       wordMat[rowCount][j] = inputWord[j] ;
       if (inputWord[j] === word[j]){
         colorMat[rowCount][j] = 'green' ;
+        match++ ;
       }
       else if (word.indexOf(inputWord[j]) !== -1 ){
         colorMat[rowCount][j] = 'yellow' ;
@@ -55,9 +70,13 @@ const Game = () => {
     setBgColor([...colorMat]) ;
     rowCount = rowCount + 1;
 
-
+    if (match===5){
+      win(true,'You won') ;
+    }
+    else if (rowCount===6){
+      win(false,`The Correct Word was ${word}`) ;
+    }
     // row out of bound problem , Show winner Status left , color change and wordlength check
-
   }
 
   return (
@@ -81,7 +100,9 @@ const Game = () => {
         </div>
       </div>
 
-      <div className="input-box flex-row">
+      <h4 style={{ color : (Para[0]===1)? 'green':'red' }} className="para">{Para[1]}</h4>
+
+      <div style={{ display: (Para[0]===1 || Para[0]===-1)? 'none':'' }} className="input-box flex-row">
         <input ref={inputRef} type="text" maxLength={5} />
         <button onClick={insertRow} >⬆️</button>
       </div>
